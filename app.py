fb06ce2379258c5690b164bcbc6610cdea2c4358
Matteo Monchiero <matteo@streamlit.io>
2019-09-27 14:34:43 -0700

    fixing broken link again

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ This is a demo of a Streamlit app that shows the Uber pickups
 geographical distribution in New York City. Use the slider
 to pick a specific hour and look at how the charts change.
 
-[See source code](https://github.com/streamlit/streamlit/blob/develop/examples/app.py)
+[See source code](https://github.com/streamlit/demo-uber-nyc-pickups/blob/master/app.py)
 """)
 
 @st.cache(persist=True)
