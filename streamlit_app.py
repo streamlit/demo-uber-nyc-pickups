3741a449afac1650c0fc2733f261321920410a06
Amanda Kelly <amanda.kelly@snowflake.com>
2022-05-17 14:38:13 -0700

    Update streamlit_app.py
    Updated the title and favicon

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ import numpy as np
 import altair as alt
 import pydeck as pdk
 
-# SETTING PAGE CONFIG TO WIDE MODE
-st.set_page_config(layout="wide")
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
 
 # LOAD DATA ONCE
 @st.experimental_singleton
