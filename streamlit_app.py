90b2cb84eaa7153c79735ffb8399208495f1c106
Zachary Blackwood <zachary@streamlit.io>
2022-05-26 09:04:27 -0400

    Add query param for tracking which hour has been selected (#23)
    * Add query param for tracking which hour has been selected
    
    * Only update session state if query param is present
    
    * Only update params on change
    
    * Use proxy session state item which only gets set explicitly by pulling from query params
    
    * Add comments
    
    * Add more detailed comment about the query parameter

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,11 @@
 
 """An example of showing geographic data."""
 
-import streamlit as st
-import pandas as pd
-import numpy as np
 import altair as alt
+import numpy as np
+import pandas as pd
 import pydeck as pdk
+import streamlit as st
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
@@ -102,9 +102,29 @@ data = load_data()
 # LAYING OUT THE TOP SECTION OF THE APP
 row1_1, row1_2 = st.columns((2, 3))
 
+# SEE IF THERE'S A QUERY PARAM IN THE URL (e.g. ?pickup_hour=2)
+# THIS ALLOWS YOU TO PASS A STATEFUL URL TO SOMEONE WITH A SPECIFIC HOUR SELECTED,
+# E.G. https://share.streamlit.io/streamlit/demo-uber-nyc-pickups/main?pickup_hour=2
+if not st.session_state.get("url_synced", False):
+    try:
+        pickup_hour = int(st.experimental_get_query_params()["pickup_hour"][0])
+        st.session_state["pickup_hour"] = pickup_hour
+        st.session_state["url_synced"] = True
+    except KeyError:
+        pass
+
+# IF THE SLIDER CHANGES, UPDATE THE QUERY PARAM
+def update_query_params():
+    hour_selected = st.session_state["pickup_hour"]
+    st.experimental_set_query_params(pickup_hour=hour_selected)
+
+
 with row1_1:
     st.title("NYC Uber Ridesharing Data")
-    hour_selected = st.slider("Select hour of pickup", 0, 23)
+    hour_selected = st.slider(
+        "Select hour of pickup", 0, 23, key="pickup_hour", on_change=update_query_params
+    )
+
 
 with row1_2:
     st.write(
